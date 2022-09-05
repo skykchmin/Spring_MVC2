b242854d066c51dc786031a3dce52b2b7325d505
skykchmin <skykchmin@naver.com>
2022-09-05 23:55:34 +0900

    [220905] feat: 타임리프 변수 유틸리티

--- thymeleaf-basic/src/main/java/basic/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/basic/thymeleaf/basic/BasicController.java
@@ -2,11 +2,14 @@ package basic.thymeleaf.basic;
 
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -46,6 +49,25 @@ public class BasicController {
         model.addAttribute("userMap", map);
 
         return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session){
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data){
+            return "Hello" + data;
+        }
+    }
+
+    @GetMapping("/date")
+    public String date(Model model){
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
     }
 
     @Data
